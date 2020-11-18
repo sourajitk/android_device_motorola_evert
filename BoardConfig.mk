0518e7461ddc72789e58965da2081cf327fd8162
Sourajit Karmakar <sourajit.karmakar@gmail.com>
2020-11-17 21:15:04 -0500

    evert: Initial Board definitions.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -13,3 +13,22 @@
 # limitations under the License.
 
 DEVICE_PATH := device/motorola/evert
+
+# Architecture
+TARGET_ARCH := arm64
+TARGET_ARCH_VARIANT := armv8-a
+TARGET_CPU_ABI := arm64-v8a
+TARGET_CPU_VARIANT := cortex-a73
+TARGET_CPU_VARIANT_RUNTIME := kryo
+
+TARGET_2ND_ARCH := arm
+TARGET_2ND_ARCH_VARIANT := armv8-a
+TARGET_2ND_CPU_ABI := armeabi-v7a
+TARGET_2ND_CPU_ABI2 := armeabi
+TARGET_2ND_CPU_VARIANT := cortex-a73
+
+# Bootloader
+TARGET_BOOTLOADER_BOARD_NAME := Evert
+
+# Platform
+TARGET_BOARD_PLATFORM := sdm660
